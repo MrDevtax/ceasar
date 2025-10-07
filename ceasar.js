6761e67e9e7aff776f6512b879e7c3f6b382322c
Dwayne Mattison <dchop123@gmail.com>
2025-10-07 12:01:26 -0400

    commit 2

--- ceasar.js
+++ ceasar.js
@@ -1,5 +1,5 @@
 <h1>Question 2</h1>
-After identifying the variables from the story, specify their primitive data types in JavaScript. Also, provide these variables with some initial values.
+//After identifying the variables from the story, specify their primitive data types in JavaScript. Also, provide these variables with some initial values.
 let letters = "abcdefghijklmnopqrstuvwxyz";
 let partyLocation = "Garden"; // String
 let shiftValue = 3; // Number
--- ceasar1.js
+++ ceasar1.js
@@ -1,5 +1,5 @@
 <h1>Question 3</h1>
-From the variables you've identified, determine which ones should be declared using const and which ones should use let.
+//From the variables you've identified, determine which ones should be declared using const and which ones should use let.
 let partyLocation = "GARDEN";
 let encryptedMessage = "";
 const shiftValue = 3;
