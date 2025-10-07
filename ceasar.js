8630018eda2bc804eb0cad947439744f992601fb
Dwayne Mattison <dchop123@gmail.com>
2025-10-07 11:55:41 -0400

    commit

---
+++ ceasar.js
@@ -0,0 +1,8 @@
+<h1>Question 2</h1>
+After identifying the variables from the story, specify their primitive data types in JavaScript. Also, provide these variables with some initial values.
+let letters = "abcdefghijklmnopqrstuvwxyz";
+let partyLocation = "Garden"; // String
+let shiftValue = 3; // Number
+let encryptedMessage = ""; // String
+let decryptedMessage = ""; // String
+let isPartySafe = false; // Boolean
---
+++ ceasar2.js
@@ -0,0 +1,4 @@
+<h1>Question 4</h1>
+Given the variable shiftValue, write a piece of code to check if its value is an integer.
+const shiftValue = 3;
+Number.isInteger(shiftValue);
