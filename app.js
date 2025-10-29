15b45ccb26f887dbd51039b84daa1b723d9fd34e
Dwayne Mattison <dchop123@gmail.com>
2025-10-29 18:59:58 -0400

    Commit8

--- app.js
+++ app.js
@@ -181,3 +181,40 @@ for (let i = 0; i < friend.length; i++)
 
 //The modulus operator, %, ensures that if the sum of the current index and the shift value surpasses the alphabet's length, it wraps around to the start. Thus, after "z", we return to "a", guaranteeing continuous encryption.
 
+
+const friend = "BRUTUS";
+const shiftValue = 3;
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function encryptLetter(letter, shift ){
+  const index = alphabet.indexOf(letter.toLowerCase());
+  const newIndex = (index + shift) % alphabet.length;
+  return alphabet[newIndex];
+}
+
+function encryptMessage(word, shift) {
+  let encryptedMessage = "";
+  for (let i = 0; i < word.length; i++) {
+    encryptedMessage += encryptLetter(word[i], shift)
+  }
+  return encryptedMessage;
+}
+
+function decryptLetter (letter, shift)
+{
+  const index = alphabet.indexOf(letter.toLowerCase());
+  const newIndex = (index - shift + alphabet.length) % alphabet.length;
+  return alphabet[newIndex];
+}
+
+function decryptMessage (word, shift)
+{
+  let decryptedMessage = "";
+  for (let i = 0; i < word.length; i++)
+  {
+    decryptedMessage += decryptLetter(word[i], shift);
+  }
+  return decryptedMessage;
+}
+
+//Yes, Caesar will get "BRUTUS" back. The decryption function is the inverse of the encryption function. When applied sequentially, they cancel each other out, restoring the original message
